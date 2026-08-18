dce1a5d48d2e3a93e8663362b5a87af6d3b024e7
João Vinicius Esquerdo de Andrade <joaovinicius0130@gmail.com>
2026-08-18 18:52:37 -0300

    chore: initialize RefrigOps project

---
+++ src/main/java/dev/joaov/refrigops/RefrigopsApplication.java
@@ -0,0 +1,13 @@
+package dev.joaov.refrigops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefrigopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefrigopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joaov/refrigops/RefrigopsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joaov.refrigops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefrigopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
